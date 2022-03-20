eff6fb2117a76457e8b3c75d86d4737b97a1b7f5
Seokjune Hong <seokjunehong@hongseogjun-ui-MacBookPro.local>
2022-03-20 13:24:21 +0900

    feat: DiceGame 추가

---
+++ DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by Seokjune Hong on 2022/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        firstImageView.image = diceArray[1]
+        secondImageView.image = diceArray[1]
+    }
+
+
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        firstImageView.image = diceArray.randomElement()
+        secondImageView.image = diceArray.randomElement()
+    }
+    
+    
+}
+
